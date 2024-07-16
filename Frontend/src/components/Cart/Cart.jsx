4c985e8c3b17b1b962aa8e2a7c453e4376d62af3
Antan Roy <antanroy502@gmail.com>
2024-07-17 01:37:05 +0530

    changing in frontend part

--- Frontend/src/components/Cart/Cart.jsx
+++ Frontend/src/components/Cart/Cart.jsx
@@ -51,7 +51,7 @@ const Cart = ({ showCart }) => {
         };
 
         try {
-            const response = await fetch("http://localhost:7000/api/create-checkout-session", {
+            const response = await fetch("https://chargex-backend.onrender.com/api/create-checkout-session", {
                 method: "POST",
                 headers: headers,
                 body: JSON.stringify(body)
